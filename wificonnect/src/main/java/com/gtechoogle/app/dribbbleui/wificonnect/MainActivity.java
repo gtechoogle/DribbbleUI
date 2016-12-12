b3ae4c07567a9c4a7b670f3d28abce1f815c597a
gtechoogle <gtechoogle@gmail.com>
2016-12-13 00:04:31 +0800

    Add scale animation for wifi button

--- wificonnect/src/main/java/com/gtechoogle/app/dribbbleui/wificonnect/MainActivity.java
+++ wificonnect/src/main/java/com/gtechoogle/app/dribbbleui/wificonnect/MainActivity.java
@@ -2,12 +2,26 @@ package com.gtechoogle.app.dribbbleui.wificonnect;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button mWifiButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWifiButton = (Button) findViewById(R.id.wifi_bt);
+        mWifiButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.wifi_bt) {
+            final Animation scale= AnimationUtils.loadAnimation(this, R.anim.wifi_button_scale);
+            view.startAnimation(scale);
+        }
     }
 }
